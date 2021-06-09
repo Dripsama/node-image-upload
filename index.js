22001c5f7d8108d552c295d7bcd219313933c072
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-06-09 22:03:07 +0530

    error handle

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const path = require("path");
 const PORT = process.env.PORT || 4000;
 
 // storage engine
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, callback) => {
+    return callback(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({
+  //filter
+  storage: storage,
+  limits: {
+    //size in bytes
+    fileSize: 1000000,
+  },
+});
 
 app.use("/profile", express.static("upload/images"));
 app.post("/upload", upload.single("images"), (req, res) => {
@@ -15,14 +33,6 @@ app.post("/upload", upload.single("images"), (req, res) => {
   });
 });
 
-function errHandler(err, req, res, next) {
-  if (err instanceof multer.MulterError) {
-    res.json({
-      success: 0,
-      message: err.message,
-    });
-  }
-}
 
 app.use(errHandler);
 app.listen(PORT, () => {
