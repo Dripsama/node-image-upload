d3baf9a8e1f8faf19954e2036cd4bcded2e6ad9f
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-06-15 22:54:20 +0530

    upload router added

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,14 @@ router.get("/", (req, res) => {
   res.json("add react webapp here");
 });
 
+//upload routes
+router.use("/profile", express.static("../upload/images"));
+router.post("/upload", upload.single("images"), (req, res) => {
+  let URL = req.protocol + "://" + req.get("host") + "/images";
+  res.json({
+    success: 1,
+    profile_url: URL + `/${req.file.filename}`,
+  });
+});
+
 module.exports = router;
