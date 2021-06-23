441d44e1018e069fe294cd9d539a384014302141
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-06-23 22:14:13 +0530

    app use cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
+const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 
 //error handler
@@ -12,11 +13,20 @@ function errHandler(err, req, res, next) {
     });
   }
 }
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+});
 //upload routes
 app.use("/", require("./routes/api"));
 
 //using errorhandler
 app.use(errHandler);
+app.use(cors);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
