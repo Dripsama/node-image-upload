e407964eee14ca7af653b64449dcb008cbccd8d6
Nikhil Saju <69810768+Dripsama@users.noreply.github.com>
2021-06-25 22:04:59 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,12 @@ router.get("/", (req, res) => {
 
 //upload routes
 router.use("/profile", express.static("../upload/images"));
-
+router.post("/upload", upload.single("images"), (req, res) => {
+  let URL = req.protocol + "://" + req.get("host") + "/images";
+  res.json({
+    success: 1,
+    profile_url: URL + `/${req.file.filename}`,
+  });
+});
 
 module.exports = router;
