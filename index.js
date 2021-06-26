8652b03c504ee588b001f39446bb7a6434857acd
Nikhil Saju <nikhilfacebook9@gmail.com>
2021-06-26 23:02:19 +0530

    fixed merge

--- index.js
+++ index.js
@@ -15,20 +15,11 @@ function errHandler(err, req, res, next) {
     });
   }
 }
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-});
 //upload routes
 app.use("/", require("./routes/api"));
 app.use("/images", express.static("upload/images"));
 
 app.use(errHandler);
-app.use(cors);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
