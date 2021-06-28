278f927679d1eec6f3a6533db962dcb63bb7c30a
Nikhil Saju <69810768+Dripsama@users.noreply.github.com>
2021-06-28 21:35:17 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,13 +7,6 @@ router.get("/", (req, res) => {
   res.json("add react webapp here");
 });
 
-//upload routes
-router.post("/upload", upload.single("images"), (req, res) => {
-  let URL = req.protocol + "://" + req.get("host") + "/images";
-  res.json({
-    success: 1,
-    profile_url: URL + `/${req.file.filename}`,
-  });
-});
+
 
 module.exports = router;
